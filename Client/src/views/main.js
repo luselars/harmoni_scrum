b566068046216b44faab19a490f526eaa2859729
junill <junill@stud.ntnu.no>
2020-01-08 13:58:46 +0100

    Added Menu to main

--- Client/src/views/main.js
+++ Client/src/views/main.js
@@ -2,14 +2,15 @@ import * as React from 'react';
 import { Component } from 'react';
 import Footer from '../components/Footer/Footer';
 import LogIn from '../components/LogIn/LogIn';
-
+import Menu from '../components/Menu/Menu';
 
 export default class Main extends Component {
     render() {
         return (
             <div>
-            <Footer/>
-            <LogIn/>
+            <Menu />
+            <LogIn />
+            <Footer />
             </div>
         )
     }
