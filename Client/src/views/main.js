a0ab0f46e976084b7c2c2c12c255db65354cca6c
Tiril Bjerkebakke Liland <tirilbl@stud.ntnu.no>
2020-01-08 13:51:53 +0100

    footer

--- Client/src/views/main.js
+++ Client/src/views/main.js
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { Component } from 'react';
+import Footer from '../components/Footer/Footer';
 
 
 export default class Main extends Component <>{
     render() {
         return (
-            <div></div>
+            <Footer/>
         )
     }
 }
