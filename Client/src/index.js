12ae8fc9b1ff50516b293715bf38f183b8792637
helenegjengedal <helenegj@stud.ntnu.no>
2020-01-08 13:41:04 +0100

    created a view for frontend development

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import * as React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import App from './App';
+//import * as serviceWorker from './serviceWorker';
+import Main from '../src/views/main';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
+//ReactDOM.render(<App />, document.getElementById('root'));
+
+const root = document.getElementById('root');
+if (root)
+  ReactDOM.render(
+    <HashRouter>
+        <div>
+        <Route exact path="/" component={Main} />
+      </div>
+    </HashRouter>,
+    root
+  );
---
+++ Client/src/views/main.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { Component } from 'react';
+
+
+export default class Main extends Component <>{
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
