bfd4dff3c6bb5e940fdf748c50500f453aa8a70c
Abilash <abilashy@ntnu.no>
2020-01-09 11:02:15 +0100

    added endpoint for update user

--- Server/dao/userDao.js
+++ Server/dao/userDao.js
@@ -14,4 +14,24 @@ module.exports = class UserDao extends Dao {
       callback
     );
   }
+
+  editUser(
+    user: json,
+    callback: (status: string, data: json) => mixed
+) {
+    super.query(
+        "UPDATE user SET email=?,name=?,tlf=?,image=?,description=? WHERE user_id = ?",
+        [
+            user.email,
+            user.name,
+            user.tlf,
+            user.image,
+            user.description,
+            user.user_id
+        ],
+        callback
+    );
+}
+
+
 };
