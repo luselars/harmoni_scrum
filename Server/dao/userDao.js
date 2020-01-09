595228277a2edda2c512645e3e2bb99eb4347541
Emir <memir0@hotmail.no>
2020-01-09 10:27:19 +0100

    Autherization with hashed and salted password work

--- Server/dao/userDao.js
+++ Server/dao/userDao.js
@@ -2,16 +2,4 @@ import "./modelDao";
 //@flow
 const Dao = require("./dao.js");
 
-module.exports = class UserDao extends Dao {
-  getUserHashAndSalt(
-    email: string,
-    password: string,
-    callback: (status: string, data: Object) => mixed
-  ) {
-    super.query(
-      "Select hash, salt from user WHERE email = ?",
-      [email],
-      callback
-    );
-  }
-};
+module.exports = class UserDao extends Dao {};
