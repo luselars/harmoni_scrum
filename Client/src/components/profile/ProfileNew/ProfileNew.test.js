4679a97643396195983e259c57cb5d47ed18984c
Stine Olava Faksdal <stineof@stud.ntnu.no>
2020-01-13 14:17:53 +0100

    testing/linting

--- Client/src/components/profile/ProfileNew/ProfileNew.test.js
+++ Client/src/components/profile/ProfileNew/ProfileNew.test.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ProfileNew from 'ProfileNew';
+import ProfileNew from 'ProfileNew.js';
+import ReactDOM from 'react-router-dom';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<ProfileNew number={0} />, div);
-    ReactDOM.unmountComponentAtNode(div);
+  const div = document.createElement('div');
+  ReactDOM.render(<ProfileNew number={0} />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
 
 Enzyme.configure({ adapter: new Adapter() });
