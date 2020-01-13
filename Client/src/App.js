894ec0eb813d373e0404ec715dd90f1de3b97ff6
Stine Olava Faksdal <stineof@stud.ntnu.no>
2020-01-13 13:29:44 +0100

    class edited to className

--- Client/src/App.js
+++ Client/src/App.js
@@ -4,45 +4,47 @@ import './App.css';
 import { withRouter } from 'react-router';
 import { Link, Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import Main from '../src/views/main';
-import Menu from './components/Menu/Menu'
-import LogIn from './components/LogIn/LogIn'
-import Footer from './components/Footer/Footer'
-import ProfileNew from './components/profile/ProfileNew/ProfileNew'
-import EventNew from './components/event/EventNew/EventNew'
-import ProfileEdit from './components/profile/ProfileEdit/ProfileEdit'
+import Menu from './components/Menu/Menu';
+import LogIn from './components/LogIn/LogIn';
+import Footer from './components/Footer/Footer';
+import ProfileNew from './components/profile/ProfileNew/ProfileNew';
+import EventNew from './components/event/EventNew/EventNew';
+import ProfileEdit from './components/profile/ProfileEdit/ProfileEdit';
 import ProfileOrganiser from './components/profile/ProfileOrganiser/ProfileOrganiser';
 import LoggedIn from './views/loggedIn';
-import Upload from "./components/Upload/Upload";
-import EventNew2 from "./components/event/EventNew/EventNew2";
+import Upload from './components/Upload/Upload';
+import EventNew2 from './components/event/EventNew/EventNew2';
 
-{/*const Main = () => <h1>Home here</h1>*/}
-const NoMatch = () => <h1>404 Not Found</h1>
+{
+  /*const Main = () => <h1>Home here</h1>*/
+}
+const NoMatch = () => <h1>404 Not Found</h1>;
 const LocationDisplay = withRouter(({ location }) => (
-    <div data-testid="location-display">{location.pathname}</div>
-))
+  <div data-testid="location-display">{location.pathname}</div>
+));
 
 function App() {
   return (
     <Router basename="/">
       <div>
-      <Link to="/"></Link>
-      <Menu/>
-      <Switch>
-        <div class="maindiv">
-          <Route exact path="/" component={Main} />
-          <Route exact path="/login" component={LogIn} />
-          <Route exact path="/upload" component={Upload} />
-          <Route exact path="/newevent" component={EventNew} />
-          <Route exact path="/newevent2" component={EventNew2} />
-          <Route exact path="/register" component={ProfileNew} />
-          <Route exact path="/profile" component={ProfileOrganiser} />
-          <Route exact path="/editprofile" component={ProfileEdit} />
-          <Route exact path="/profile/summary" component={LoggedIn} />
-        </div>
-        <Route component={NoMatch} />
-      </Switch>
-      <Footer/>
-      <LocationDisplay />
+        <Link to="/"></Link>
+        <Menu />
+        <Switch>
+          <div className="maindiv">
+            <Route exact path="/" component={Main} />
+            <Route exact path="/login" component={LogIn} />
+            <Route exact path="/upload" component={Upload} />
+            <Route exact path="/newevent" component={EventNew} />
+            <Route exact path="/newevent2" component={EventNew2} />
+            <Route exact path="/register" component={ProfileNew} />
+            <Route exact path="/profile" component={ProfileOrganiser} />
+            <Route exact path="/editprofile" component={ProfileEdit} />
+            <Route exact path="/profile/summary" component={LoggedIn} />
+          </div>
+          <Route component={NoMatch} />
+        </Switch>
+        <Footer />
+        <LocationDisplay />
       </div>
     </Router>
   );
