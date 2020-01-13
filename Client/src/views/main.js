b5724754a65dcf22260d28cbd54a133b191531ba
Tiril Bjerkebakke Liland <tirilbl@stud.ntnu.no>
2020-01-13 14:23:55 +0100

    EventDeleted done

--- Client/src/views/main.js
+++ Client/src/views/main.js
@@ -5,13 +5,13 @@ import Menu from '../components/Menu/Menu';
 import EventList from '../components/event/EventList/EventList';
 import ProfileOrganiser from '../components/profile/ProfileOrganiser/ProfileOrganiser';
 import Filter from '../components/Filter/Filter';
+import EventDeleted from '../components/event/EventDeleted/EventDeleted';
 
 export default class Main extends Component {
     render() {
         return (
             <div>
-            <Filter />
-            <EventList />
+            <EventDeleted />
             </div>
         )
     }
