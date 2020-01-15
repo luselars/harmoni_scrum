fcb3f61dd1fd6f4655535e583cc7e025626181cd
emir derouiche <memir0@hotmail.no>
2020-01-15 15:18:23 +0100

    Added auth middleware to user

--- Server/src/routes/userRoutes.js
+++ Server/src/routes/userRoutes.js
@@ -1,10 +1,38 @@
 // @flow
 import express from 'express';
 const path = require('path');
+const tokenDecoder = require('./tokenDecoder');
+let td = new tokenDecoder();
 
 const userDao = require('../../dao/userDao.js');
 let dao = new userDao();
 let router = express.Router();
+
+// Middleware for organiser activities BRUK DENNE FOR USER OGSÅ
+router.use('', (req, res, next) => {
+  var token = req.headers['x-access-token'];
+  td.decode(token, (err, decoded) => {
+    if (err) {
+      res.status(401);
+      res.json({
+        error: err,
+      });
+    } else {
+      if (decoded.type == 'user') {
+        console.log('Token ok: ' + decoded.username);
+        req.email = decoded.username;
+        req.uid = decoded.id;
+        next();
+      } else {
+        console.log('Token NOT ok');
+        res.status(401);
+        res.json({
+          error: 'Not authorized, you do not have access to this action',
+        });
+      }
+    }
+  });
+});
 
 // Edit a specific user
 router.put('', (req: { body: JSON }, res: express$Response) => {
