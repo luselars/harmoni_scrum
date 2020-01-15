530fcc3a7548879a67552a2984a95be20b5470c5
LuseLars <larsoestmo@gmail.com>
2020-01-15 14:03:16 +0100

    fikset at den ikke finner navn på event

--- Server/dao/organiserDao.js
+++ Server/dao/organiserDao.js
@@ -7,10 +7,10 @@ module.exports = class OrganiserDao extends Dao {
   getEvent(
     event_id: number,
     organiser_id: number,
-    callback: (status: string, data: Object) => mixed,
+    callback: (status: string, data: Event) => mixed,
   ) {
     var queryString =
-      'SELECT e.*, l.* FROM event e LEFT JOIN event_organiser eo ON e.event_id = eo.event_id LEFT JOIN location l ON l.location_id = e.location_id WHERE eo.organiser_id = ? AND e.event_id = ?';
+      'SELECT e.*, l.location_id, l.name as location_name, l.address, l.postcode FROM event e LEFT JOIN event_organiser eo ON e.event_id = eo.event_id LEFT JOIN location l ON l.location_id = e.location_id WHERE eo.organiser_id = ? AND e.event_id = ?';
     super.query(queryString, [organiser_id, event_id], callback);
   }
 
