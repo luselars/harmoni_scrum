8bee350fb8b4a46fff781219bb67eb1cc0de32d4
emir derouiche <memir0@hotmail.no>
2020-01-17 14:12:58 +0100

    Added pages to front page

--- Client/src/components/event/EventList/EventList.js
+++ Client/src/components/event/EventList/EventList.js
@@ -8,6 +8,7 @@ import './stylesheet.css';
 import { string } from 'prop-types';
 import { PublicService } from '../../../services/publicService';
 import { UserService } from '../../../services/userService';
+import ReactPaginate from 'react-paginate';
 
 let dates = ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Des'];
 let events: Event[] = [];
@@ -23,57 +24,85 @@ export default class EventList extends Component<Props, State> {
       status: localStorage.getItem('token') === null,
       organiser_id: 0,
       organiser: organiser,
+      offset: 0,
     };
   }
+
+  handlePageClick = data => {
+    let selected = data.selected;
+    let offset = Math.ceil(selected * 7);
+
+    this.setState({ offset: offset });
+  };
+
   render() {
     return (
       <div>
-        {this.state.events.map(event => (
-          <div className="card">
-            <div className="card-body bg-light">
-              <div className="container bg-light">
-                <div className="row justify-content-md-center align-items-center">
-                  <div id="date" className="col-2 text-center">
-                    <h3 className="datenumber">{event.start.slice(8, 10)}</h3>
-                    <h3 className="datemonth">{dates[event.start.slice(5, 7) - 1]}</h3>
-                  </div>
-                  <div id="eventinfo" className="col-8">
-                    <h5 class="eventtitle">{event.name}</h5>
-                    <p className="eventlistp">
-                      <a className="eventdescription">Sted: </a>
-                      {event.venue}
-                    </p>
-                    <p className="eventlistp">
-                      <a className="eventdescription">Tid: </a>
-                      {event.start.slice(11, 16)}
-                    </p>
-                  </div>
-                  <div id="eventbtn" className="col text-right">
-                    {this.state.status ? (
-                      <button
-                        className="btn btn-success bg-green"
-                        id="moreinfo"
-                        onClick={() => (window.location.href = '/event/' + event.event_id)}
-                      >
-                        {' '}
-                        Mer info
-                      </button>
-                    ) : (
-                      <button
-                        className="btn btn-success bg-green"
-                        id="moreinfo"
-                        onClick={() => (window.location.href = '/orgevent/' + event.event_id)}
-                      >
-                        {' '}
-                        Mer info
-                      </button>
-                    )}
+        {this.state.events.map((event, index) =>
+          index >= this.state.offset && index - this.state.offset < 7 ? (
+            <div className="card">
+              <div className="card-body bg-light">
+                <div className="container bg-light">
+                  <div className="row justify-content-md-center align-items-center">
+                    <div id="date" className="col-2 text-center">
+                      <h3 className="datenumber">{event.start.slice(8, 10)}</h3>
+                      <h3 className="datemonth">{dates[event.start.slice(5, 7) - 1]}</h3>
+                    </div>
+                    <div id="eventinfo" className="col-8">
+                      <h5 class="eventtitle">{event.name}</h5>
+                      <p className="eventlistp">
+                        <a className="eventdescription">Sted: </a>
+                        {event.venue}
+                      </p>
+                      <p className="eventlistp">
+                        <a className="eventdescription">Tid: </a>
+                        {event.start.slice(11, 16)}
+                      </p>
+                    </div>
+                    <div id="eventbtn" className="col text-right">
+                      {this.state.status ? (
+                        <button
+                          className="btn btn-success bg-green"
+                          id="moreinfo"
+                          onClick={() => (window.location.href = '/event/' + event.event_id)}
+                        >
+                          {' '}
+                          Mer info
+                        </button>
+                      ) : (
+                        <button
+                          className="btn btn-success bg-green"
+                          id="moreinfo"
+                          onClick={() => (window.location.href = '/orgevent/' + event.event_id)}
+                        >
+                          {' '}
+                          Mer info
+                        </button>
+                      )}
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          ) : (
+            ''
+          ),
+        )}
+        <div>
+          <ReactPaginate
+            previousLabel={'previous'}
+            nextLabel={'next'}
+            breakLabel={'...'}
+            breakClassName={'break-me'}
+            pageCount={this.state.pageCount}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={5}
+            onPageChange={this.handlePageClick}
+            containerClassName={'pagination'}
+            subContainerClassName={'pages pagination'}
+            activeClassName={'active'}
+          />
+        </div>
       </div>
     );
   }
@@ -103,7 +132,10 @@ export default class EventList extends Component<Props, State> {
         .then(events => {
           console.log('welcome to the frontpage');
           console.log(events);
-          this.setState({ events: events.data });
+          this.setState({
+            events: events.data,
+            pageCount: Math.ceil(events.data.length / 7),
+          });
         })
         .catch((error: Error) => alert(error.message));
     }
