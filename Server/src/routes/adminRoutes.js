63da04b9c7f59677c31efc52cfbc3392f2d41f9b
LuseLars <larsoestmo@gmail.com>
2020-01-20 14:07:31 +0100

    endepunkter for admin, må fortsatt autoriseres

--- Server/src/routes/adminRoutes.js
+++ Server/src/routes/adminRoutes.js
@@ -4,10 +4,28 @@ const path = require('path');
 const tokenDecoder = require('./tokenDecoder');
 let td = new tokenDecoder();
 
-const userDao = require('../../dao/userDao.js');
+const userDao = require('../../dao/adminDao.js');
 let dao = new userDao('mysql-ait.stud.idi.ntnu.no', 'larsoos', 'S6yv7wYa', 'larsoos');
 let router = express.Router();
 
 //TODO authorization.
 // only admin should be able to use this
 // add routes /organisers, and /verify/:organiser_id
+
+
+
+// verify organiser
+router.put('/verify/:organiser_id', (req: express$Request, res: express$Response) => {
+    dao.verifyOrganiser(req.params.organiser_id, (status, data) => {
+        res.status(status);
+        res.send(data);
+    });
+});
+
+// get all the organisers
+router.put('/organisers', (req: express$Request, res: express$Response) => {
+    dao.getOrganisers((status, data) => {
+        res.status(status);
+        res.send(data);
+    });
+});
