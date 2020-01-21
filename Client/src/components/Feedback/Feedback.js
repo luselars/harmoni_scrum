f2480a2a89694b70ef4bbc9e8a061b490cee94ae
Tiril <tirilbl@stud.ntnu.no>
2020-01-21 09:12:38 +0100

    feedback button

---
+++ Client/src/components/Feedback/Feedback.js
@@ -0,0 +1,9 @@
+// @flow
+import * as React from 'react';
+import { Component } from 'react';
+
+export default class Feedback extends Component<Props, State> {
+  render() {
+    return <div>halla</div>;
+  }
+}
