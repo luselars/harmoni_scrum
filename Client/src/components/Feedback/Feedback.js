ca72f7a4bb00fc35a7286a232d901de68e4899f6
Stine Olava Faksdal <stineof@stud.ntnu.no>
2020-01-21 15:59:30 +0100

    noen har glemt flow

--- Client/src/components/Feedback/Feedback.js
+++ Client/src/components/Feedback/Feedback.js
@@ -2,6 +2,10 @@
 import * as React from 'react';
 import { Component } from 'react';
 
+type Props = {};
+
+type State = {};
+
 export default class Feedback extends Component<Props, State> {
   render() {
     return <div>halla</div>;
