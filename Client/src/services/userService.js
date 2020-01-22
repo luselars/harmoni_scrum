b66b336607c6f917f48ed455ba3306d156e52593
emir derouiche <memir0@hotmail.no>
2020-01-22 14:26:06 +0100

    Added config to userservice

--- Client/src/services/userService.js
+++ Client/src/services/userService.js
@@ -3,34 +3,46 @@ import axios, { AxiosPromise } from 'axios';
 import { User } from './modelService';
 const url_base = 'http://localhost:4000/user';
 
+const config = {
+  headers: {
+    'x-access-token': localStorage.getItem('token'),
+  },
+};
+
 export class UserService {
   static getMyProfile(): AxiosPromise<User> {
     let url = url_base + '/myprofile';
     let token = localStorage.getItem('token');
-    return axios.get(url, { headers: { 'x-access-token': token } });
+    return axios.get(url, config);
   }
 
   static deleteUser(id: number): AxiosPromise<User> {
     let url = url_base + '/' + id;
     let token = localStorage.getItem('token');
-    return axios.delete<Object>(url, { headers: { 'x-access-token': token } });
+    return axios.delete<Object>(url, config);
   }
 
   static getMyEvents(): AxiosPromise<Event[]> {
     let url = url_base + '/myevents';
     let token = localStorage.getItem('token');
-    return axios.get(url, { headers: { 'x-access-token': token } });
+    return axios.get(url, config);
+  }
+
+  static getEvent(): AxiosPromise<Event> {
+    let url = url_base + '/myevent';
+    let token = localStorage.getItem('token');
+    return axios.get(url, config);
   }
 
   static editUser(user: User): AxiosPromise<User> {
     let url = url_base + '/myprofile';
     let token = localStorage.getItem('token');
-    return axios.put<Object>(url, user, { headers: { 'x-access-token': token } });
+    return axios.put<Object>(url, user, config);
   }
 
   static editArtistname(artist_name: string): AxiosPromise<User> {
     let url = url_base + '/artistname';
     let token = localStorage.getItem('token');
-    return axios.put<Object>(url, { artist_name }, { headers: { 'x-access-token': token } });
+    return axios.put<Object>(url, { artist_name }, config);
   }
 }
