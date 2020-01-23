a78c936617e425a16fba5735aba043a8d181efd6
Stine Olava Faksdal <stineof@stud.ntnu.no>
2020-01-23 15:45:18 +0100

    test passed

--- Client/src/components/event/EventDetailsLoggedIn/EventDetailsLoggedIn.test.js
+++ Client/src/components/event/EventDetailsLoggedIn/EventDetailsLoggedIn.test.js
@@ -4,13 +4,7 @@ import ReactDOM from 'react-dom';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<EventDetailsLoggedIn />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-
 Enzyme.configure({ adapter: new Adapter() });
 it('shallow renders without crashing', () => {
-  shallow(<EventDetailsLoggedIn match={{ params: { id: number } }} />);
+  const wrapper = shallow(<EventDetailsLoggedIn match={{ params: { id: 0 } }} />);
 });
