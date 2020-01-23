8161da0091e25484c90936eaf109d9af564b2a69
Stine Olava Faksdal <stineof@stud.ntnu.no>
2020-01-23 16:19:46 +0100

    Profile test passed

--- Client/src/components/profile/Profile/Profile.test.js
+++ Client/src/components/profile/Profile/Profile.test.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import ProfileOrganiser from 'ProfileOrganiser.js';
+import Profile from './Profile.js';
 import ReactDOM from 'react-router-dom';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<ProfileOrganiser number={0} />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-
 Enzyme.configure({ adapter: new Adapter() });
 it('shallow renders without crashing', () => {
-  shallow(<ProfileOrganiser />);
+  const wrapper = shallow(<Profile />);
 });
