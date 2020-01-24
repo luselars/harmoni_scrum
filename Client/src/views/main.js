47170b1c2d462b699fc904076899c2b0959d08f1
helenegjengedal <helenegj@stud.ntnu.no>
2020-01-24 21:49:05 +0100

    imported searchbar in main

--- Client/src/views/main.js
+++ Client/src/views/main.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Component } from 'react';
 import EventList from '../components/event/EventList/EventList';
+import SearchBar from '../components/SearchBar/SearchBar';
 
 /** Class for frontpage view. */
 export default class Main extends Component<{}, State> {
