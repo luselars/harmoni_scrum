b46f439c334203be4a141ca11fd1fb31da02beef
Lovster <sebastian.elvemo@gmail.com>
2020-01-24 12:49:02 +0100

    Commenting on serverside items.

--- Server/src/server.js
+++ Server/src/server.js
@@ -12,7 +12,7 @@ const cors = require('cors');
 
 const PORT = 4000;
 
-// Routes
+// Routes (all separated into relevant files)
 let publicRoutes = require('./routes/publicRoutes');
 let organiserRoutes = require('./routes/organiserRoutes');
 let userRoutes = require('./routes/userRoutes');
@@ -20,23 +20,13 @@ let adminRoutes = require('./routes/adminRoutes');
 
 let app = express();
 
-// TODO snakk om maks-filstørrelse, legg inn begrensning i front-end også
+// Max file size set to 50mb for uploaded files.
 app.use(bodyParser.json({ limit: '50mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 // CORS
 app.use(cors());
 app.options('*', cors());
-// app.use(function(req, res, next) {
-//   // Website you wish to allow to connect
-//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-//   // Request methods you wish to allow
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//   // Request headers you wish to allow
-//   res.setHeader('Access-Control-Allow-Headers', 'content-type,x-access-token');
-//   // Pass to next layer of middleware
-//   next();
-// });
 
 // Dependencies
 app.use(express.json()); // For parsing application/json
