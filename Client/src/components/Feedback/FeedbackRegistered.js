89ea228442a6bcfd3111eee6d94458ddedea26d4
junill <junill@stud.ntnu.no>
2020-01-24 10:32:19 +0100

    comments client and login alert

--- Client/src/components/Feedback/FeedbackRegistered.js
+++ Client/src/components/Feedback/FeedbackRegistered.js
@@ -2,6 +2,7 @@
 import * as React from 'react';
 import { Component } from 'react';
 
+//Component for feedback confirmation message
 export default class FeedbackRegistered extends Component<{}, {}> {
   render() {
     return (
