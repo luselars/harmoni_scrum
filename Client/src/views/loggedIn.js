eda90b29797e99cf3795f351de6bdfcb115cbf91
junill <junill@stud.ntnu.no>
2020-01-24 13:27:35 +0100

    comments

--- Client/src/views/loggedIn.js
+++ Client/src/views/loggedIn.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { Component } from 'react';
-import ProfileSummary from '../components/profile/ProfileSummary/ProfileSummary';
 import EventList from '../components/event/EventList/EventList';
 import SearchBar from '../components/SearchBar/SearchBar';
 
--- Client/src/views/main.js
+++ Client/src/views/main.js
@@ -1,13 +1,6 @@
 import * as React from 'react';
 import { Component } from 'react';
-//import Footer from '../components/Footer/Footer';
-//import Menu from '../components/Menu/Menu';
 import EventList from '../components/event/EventList/EventList';
-//import ProfileOrganiser from '../components/profile/ProfileOrganiser/ProfileOrganiser';
-//import Filter from '../components/Filter/Filter';
-//import SearchBar from '../components/SearchBar/SearchBar';
-//import filterStore from '../services/filterStore';
-//import { string } from 'prop-types';
 
 export default class Main extends Component<{}, State> {
   render() {
